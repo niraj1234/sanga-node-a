ab8967346cc5fb035304ff8b92fa376a894b8f05
niraj1234 <niraj.webtech@gmail.com>
2025-01-22 02:26:40 +0530

    Basic server created and routes demonstrated

---
+++ http-a/routes.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const server = http.createServer((req,res) => {
+
+    const url  = req.url;
+    if(url === "/"){
+        res.writeHead(200 , {"Content-Type":"text/plain"});
+        res.end("Home Page");
+    } else if(url === "/projects"){
+        res.writeHead(200 , {"Content-Type":"text/plain"});
+        res.end("Project List");
+    } else {
+        res.writeHead(404 , {"Content-Type":"text/plain"});
+        res.end("Page not found.");
+    }
+
+ }
+)
+
+const PORT = 3000 ;
+server.listen(PORT , ()=>{
+    console.log(`Server is running on ${PORT}`);
+})
