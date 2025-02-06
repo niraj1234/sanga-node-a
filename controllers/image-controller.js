7de85cd66df75e4244ac6b96627d0d4595080d81
niraj1234 <niraj.webtech@gmail.com>
2025-02-07 01:23:07 +0530

    Pagination with Sorting done

--- controllers/image-controller.js
+++ controllers/image-controller.js
@@ -41,28 +41,42 @@ const uploadImageController = async (req,res) => {
 }
 
 const fetchAllImages = async (req,res) => {
-    try {
-        const images = await Image.find({});
-        if(!images){
-            res.status(400).json({
-                success : false,
-                message : "Image not found" 
-            })
-        } else{
-            res.status(200).json({
-                success : true,
-                message : `Total images found ${images.length} ` ,
-                data : images
-            })
-        }        
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            success : false ,
-            message : 'Something went wrong on server side Image Controller'
-        })            
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 2;
+    const skip = (page - 1) * limit;
+
+    const sortBy = req.query.sortBy || "createdAt";
+    const sortOrder = req.query.sortOrder === "asc" ? 1 : -1;
+    const totalImages = await Image.countDocuments();
+    const totalPages = Math.ceil(totalImages / limit);
+
+    const sortObj = {};
+    sortObj[sortBy] = sortOrder;
+
+    const images = await Image.find()
+                              .sort(sortObj)
+                              .skip(skip)
+                              .limit(limit);
+
+    if (images) {
+      res.status(200).json({
+        success: true,
+        currentPage: page,
+        totalPages: totalPages,
+        totalImages: totalImages,
+        data: images,
+      });
     }
-}
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong! Please try again",
+    });
+  }
+};
+
 
 
 // Delete image from MongoDB and Cloudinary
